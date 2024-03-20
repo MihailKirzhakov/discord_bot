ea71e4bfe4208428be53bfad73e3758dbfa7df03
Mihail Kirzhakov <kirzhakovma@mail.ru>
2024-03-20 19:11:31 +0300

    Added circle to command

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,35 +1,26 @@
 import discord
 from discord.ext import commands
-from discord.ui import View
+from discord.ui import View, Button
 from discord.ui.item import Item
-
-
-class AucButton(View):
-    def __init__(
-            self,
-            *items: Item,
-            timeout: float | None = None,
-            disable_on_timeout: bool = False,
-            count: int
-    ):
-        super().__init__(
-            *items,
-            timeout=timeout,
-            disable_on_timeout=disable_on_timeout
-        )
-        self.count = count
-
-    for i in range(self.count):
-        @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary)
-        async def button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-            button.label = interaction.user.display_name
-            await interaction.response.edit_message(view=self)
 
 
 @commands.slash_command()
 async def go_auc(ctx: discord.ApplicationContext, count: int):
-    await ctx.respond(view=AucButton(count=count))
+    button_manager = View()
+    for i in range(count):
+        auc_button: discord.ui.Button = Button(label='300')
+        button_manager.add_item(auc_button)
+        auc_button.callback = button_callback(auc_button, button_manager)
+    await ctx.respond(view=button_manager)
 
+
+def button_callback(button: discord.ui.Button, view):
+    async def inner(interaction: discord.Interaction):
+        button.label = interaction.user.display_name
+        await interaction.response.edit_message(view=view)
+    return inner
+
+    
 
 def setup(bot: discord.Bot):
     bot.add_application_command(go_auc)
--- bot/test_algs.py
+++ bot/test_algs.py
@@ -1,20 +1,15 @@
-#         user_name = interaction.user.display_name
-#         thousand = 'K'
-#         million = 'M'
-#         original_label = button.label.split()
-#         current_label = float(original_label[0])
-#         if 300 <= current_label < 900:
-#             current_label += 100
-#             button.label = f'{int(current_label)} {thousand} {user_name}'
-#         elif current_label == 900:
-#             current_label += 100
-#             current_label /= 1000
-#             button.label = f'{round(current_label)} {million} {user_name}'
-#         else:
-#             current_label += 0.1
-#             if current_label.is_integer():
-#                 button.label = f'{round(current_label)} {million} {user_name}'
-#             else:
-#                 button.label = f'{round(current_label, 1)} {million} {user_name}'
-#         button.style = discord.ButtonStyle.blurple
-#         await interaction.response.edit_message(view=self)
+class AucButton(discord.ui.View):
+    def __init__(self, count, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for _ in range(count):
+            btn = discord.ui.Button(label="Click me!")
+            btn.callback = self.custom_callback(btn)
+            self.add_item(btn)
+
+    @staticmethod
+    def custom_callback(button: discord.ui.Button):
+        async def inner(interaction: discord.Interaction):
+            button.label = interaction.user.display_name
+            await interaction.edit_original_message(view=button.view)
+        return inner
