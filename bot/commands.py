5b9e468a23b068ffc9a87151a0867204d7a4bca0
Kirzhakov Mihail <kirzhakovma@mail.ru>
2024-04-06 21:55:20 +0300

    Added randomiser

--- bot/commands.py
+++ bot/commands.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from functions import rand_choice
 
 
 @commands.slash_command()
@@ -8,7 +9,27 @@ async def gogo(ctx: discord.ApplicationContext):
 
 
 @commands.slash_command()
-@commands.has_role('Аукционер')
+@commands.has_role('Аукцион')
+async def random(ctx: discord.ApplicationContext, nicknames: discord.Option(
+    str,
+    description='Впиши ники через пробел',
+    name_localizations={'ru': 'никнэймы'}
+)): # type: ignore
+    """Команда вызывающая рандомайзер"""
+    await ctx.respond(f'Результаты:\n{rand_choice(nicknames)}')
+
+@random.error
+async def on_application_command_error(ctx: discord.ApplicationContext, error):
+    if isinstance(error, commands.errors.NoPrivateMessage):
+        await ctx.respond('Эта команда не может быть вызвана через ЛС')
+    elif isinstance(error, commands.errors.MissingRole):
+        await ctx.respond(f'{ctx.author.mention} ты, дружочек, не достоин просить меня это сделать!')
+    else:
+        return error
+
+
+@commands.slash_command()
+@commands.has_role('Аукцион')
 async def greet(ctx: discord.ApplicationContext, name: str):
     await ctx.respond(f'Ну привет, {name}! Тестим')
 
@@ -26,3 +47,4 @@ async def on_application_command_error(ctx: discord.ApplicationContext, error):
 def setup(bot: discord.Bot):
     bot.add_application_command(gogo)
     bot.add_application_command(greet)
+    bot.add_application_command(random)
