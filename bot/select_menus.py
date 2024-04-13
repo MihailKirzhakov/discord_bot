3419306d9bba0cec59df3f46a3811b0a41eba622
Михаил Киржаков <kirzhakovma@mail.ru>
2024-04-13 22:45:38 +0300

    Made select_menus.py

--- bot/select_menus.py
+++ bot/select_menus.py
@@ -1,42 +1,32 @@
 import discord
 
 from discord.ext import commands
-from discord.ui import Modal, InputText, View, Button, button
+from discord.ui import Modal, InputText, View, button
 
-
-class ApplicationButton(View):
-
-    @button(label='Заполни форму', style=discord.ButtonStyle.green, emoji='📋')
-    async def callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_modal(RoleApplication())
 
 
 class RoleButton(View):
 
+    def __init__(self, user, channel: discord.TextChannel, timeout: float | None = None):
+        super().__init__(timeout=timeout)
+        self.user = user
+        self.channel = channel
+
     @button(label='Выдать старшину', style=discord.ButtonStyle.green)
     async def callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        
-        await interaction.response
-
-# def access_button(user):
-#     button_manager = View(timeout=None)
-#     button: discord.ui.Button = Button(
-#         label='Выдать старшину',
-#         style=discord.ButtonStyle.green
-#     )
-
-#     async def button_callback(interaction: discord.Interaction):
-#         guild = interaction.guild
-#         role = guild.get_role(1227886432002117734)
-#         await user.add_roles(role)
-#     button.callback = button_callback
-#     button_manager.add_item(button)
-
+        role = discord.utils.get(interaction.guild.roles, id=1222655185055252581)
+        self.disable_all_items()
+        await self.user.add_roles(role)
+        await interaction.response.edit_message(
+            view=self
+        )
 
 
 class RoleApplication(Modal):
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, channel: discord.TextChannel, *args, **kwargs):
         super().__init__(*args, **kwargs, title='Заявка на выдачу роли')
+        self.channel = channel
 
         self.add_item(
             InputText(
@@ -47,24 +37,54 @@ class RoleApplication(Modal):
 
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user
+        member = discord.utils.get(interaction.guild.members, id=user.id)
         nickname = self.children[0].value
-        await interaction.respond(
-            'Твой запрос принят! Дождись выдачи роли',
-            empheral=True
+        embed = discord.Embed(
+            title=f'Заявка на доступ',
+            description='Думаю над текстом',
+            color=0x6e00ff
         )
+        embed.set_author(name=member.display_name, icon_url='https://avatars.mds.yandex.net/i?id=25468a149adb4b493e2ec75c6dfc0f6a7b318532-7664914-images-thumbs&n=13')
+        embed.add_field(name='Текущий класс', value='Здесь будет отображаться спарсеный класс', inline=True),
+        embed.add_field(name='Текущий ГС', value='Здесь будет отображаться спарсеный ГС', inline=True),
+        embed.add_field(name='Текущий уровень наследия богов', value='Здесь будет отображаться лвл НБ', inline=True),
+        embed.set_thumbnail(url=member.avatar)
         await interaction.respond(
-            f'Принял запрос!\n'
-            f'{user.mention} просит выдать роль\n'
-            f'Игровой ник: {nickname}'
+            '_Твой запрос принят! Дождись выдачи роли_',
+            ephemeral=True
         )
+        await user.edit(nick=nickname)
+        await self.channel.send(
+            view=RoleButton(user=user, channel=self.channel),
+            embed=embed
+        )
+
+
+class ApplicationButton(View):
+
+    def __init__(self, channel: discord.TextChannel, timeout: float | None = None):
+        super().__init__(timeout=timeout)
+        self.channel = channel
+
+    @button(label='Заполни форму', style=discord.ButtonStyle.green, emoji='📋')
+    async def callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_modal(RoleApplication(channel=self.channel))
 
 
 @commands.slash_command()
 @commands.has_any_role('📣Казначей📣', '🛡️Офицер🛡️')
-async def role_application(ctx: discord.ApplicationContext):
+async def role_application(
+    ctx: discord.ApplicationContext,
+    channel: discord.Option(
+        discord.TextChannel,
+        description='Выбери канал в который будут отправляться заявки',
+        name_localizations={'ru': 'название_канала'}
+    )  # type: ignore
+):
+    """Команда для вызова кнопки, которая обрабатывает запросы на доступ"""
     await ctx.respond(
-        'Привет!\n Заполни форму для получения доступа',
-        view=ApplicationButton()
+        '_**Привет!\n Заполни форму для получения доступа**_',
+        view=ApplicationButton(channel=channel)
     )
 
 
