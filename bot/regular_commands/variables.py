79523212ce9c2d2afa23756fefc35dbf9ce0a80a
Михаил Киржаков <kirzhakovma@mail.ru>
2024-05-02 18:47:07 +0300

    changed the url of the picture of the cubes, added a phrase about technical work, added the url of the picture of the wrench

--- bot/regular_commands/variables.py
+++ bot/regular_commands/variables.py
@@ -2,6 +2,12 @@
 
 DEAFAULT_RANDOMISE_VALUE = '1-100'
 
-PLAYING_DICES_URL_ICON = 'https://media.discordapp.net/attachments/1182584501147279491/1232412043437539425/829025-middle.png?ex=66295ca8&is=66280b28&hm=ce10f2e672b7418b2b9cb698815fb5af2f09cfc0fed51ae229226d6faddf89ab&=&format=webp&quality=lossless&width=550&height=343'
+PLAYING_DICES_URL_ICON = 'https://media.discordapp.net/attachments/1182584501147279491/1235613464194584689/dice_PNG64.png?ex=66350236&is=6633b0b6&hm=d7df1ef64c9d647e0fa292199bf7be5160d2f95d6fecc4aa61622fe4bd9649e1&=&format=webp&quality=lossless&width=951&height=640'
 
 WRONG_PARMS = 'Неверно заданы параметры, повтори снова!🔁'
+
+TEСHNICAL_WORKS = (
+    '_Скоро запустим!_👌'
+)
+
+WRENCH_IMAGE_URL = 'https://media.discordapp.net/attachments/1182584501147279491/1235611311896002600/1672451474_flomaster-club-p-gaechnii-klyuch-risunok-dlya-detei-vkontak-16.png?ex=66350035&is=6633aeb5&hm=0055b69a7b0ba64d904bc8e2b1fb4057ae3ee781312920e5b6508b036b707d35&=&format=webp&quality=lossless&width=960&height=480'
