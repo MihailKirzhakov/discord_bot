a2d65f4ff97fce1d76084cf847d7c4b0c08a143c
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-01-15 10:19:25 +0300

    `Update database_url in Settings class to default to 'sqlite+aiosqlite:///./discord_bot.db'`

--- bot/core/config.py
+++ bot/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: Optional[str] = None
+    database_url: Optional[str] = 'sqlite+aiosqlite:///./discord_bot.db'
     token: Optional[str] = None
     debug_server_id: Optional[int] = None
 
