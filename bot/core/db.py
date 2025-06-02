63fef2a9ae6cee0eedc370a349485ed2982a38a4
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-06-02 11:42:20 +0300

    refact variables, update imports

--- bot/core/db.py
+++ bot/core/db.py
@@ -1,19 +1,24 @@
-from typing import Annotated
+from typing import Annotated, TypeVar
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import (
-    declared_attr, DeclarativeBase, mapped_column, Mapped
-)
+from sqlalchemy.orm import declared_attr, DeclarativeBase,  mapped_column
 
 from .config import settings
+
+
+intpk = Annotated[int, mapped_column(primary_key=True, unique=True)]
+strpk = Annotated[str, mapped_column(primary_key=True, unique=True)]
+int_uniq = Annotated[int, mapped_column(unique=True)]
+str_uniq = Annotated[str, mapped_column(unique=True)]
 
 
 class Base(DeclarativeBase):
 
     @declared_attr
     def __tablename__(cls):
-        return cls.__name__.lower()
+        return cls.__name__
 
 
+ModelType = TypeVar('ModelType', bound=Base)
 async_engine = create_async_engine(settings.database_url)
 async_session_factory = async_sessionmaker(async_engine)
