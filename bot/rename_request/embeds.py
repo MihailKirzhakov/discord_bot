56fbaefecf8dd6deebdb5bd887b0d5759acf91b2
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-07-09 16:47:21 +0300

    переделаны функции для формирования Embed

--- bot/rename_request/embeds.py
+++ bot/rename_request/embeds.py
@@ -3,49 +3,57 @@ import discord
 from core import DENIED_IMAGE_URL, ACCESS_IMAGE_URL, RENAME_IMAGE_URL
 
 
-def rename_embed(user: str, nickname: str) -> discord.Embed:
+def rename_embed(
+    old_nickname: str,
+    jump_url: str,
+    new_nickname: str
+) -> discord.Embed:
     """
     Функция для создания вложения с переименованием.
     """
     embed = discord.Embed(
-        title='_**ЗАПРОС НА СМЕНУ НИКА**_',
         description=(
-            f'_**{user}** просит изменить никнейм на **{nickname}**!\n'
+            f'_Просит изменить никнейм на **{new_nickname}**!\n'
             f'**УБЕДИТЕЛЬНАЯ** просьба проверить сперва его ренейм в игре!\n'
             f'Если не совпадает, то отказываем, пока не поменяет в игре!_'
         ),
         color=0xfffb00
     )
+    embed.set_author(name=old_nickname, url=jump_url)
     embed.set_thumbnail(url=RENAME_IMAGE_URL)
     return embed
 
 
-def changed_rename_embed(user, nickname) -> discord.Embed:
+def changed_rename_embed(
+    old_nickname: str,
+    jump_url: str,
+    new_nickname: str
+) -> discord.Embed:
     """
     Функция для создания вложения с выполненным переименованием.
     """
     embed = discord.Embed(
-        title='_**Никнейм ИЗМЁНЕН**_',
         description=(
-            f'_**{user}** изменил никнейм на **{nickname}**!_\n'
+            f'_изменил никнейм на **{new_nickname}**!_\n'
         ),
         color=0xfffb00
     )
+    embed.set_author(name=old_nickname, url=jump_url)
     embed.set_thumbnail(url=ACCESS_IMAGE_URL)
     return embed
 
 
-def denied_rename_embed(user: str) -> discord.Embed:
+def denied_rename_embed(old_nickname: str, jump_url: str) -> discord.Embed:
     """
     Функция для создания вложения с отказом в ренейме.
     """
     embed = discord.Embed(
-        title='_**Никнейм НЕ ИЗМЁНЕН**_',
         description=(
-            f'_**{user}** НЕ изменил ник в игре, ожидаем!_\n'
+            f'_**НЕ** изменил ник в игре, ожидаем!_\n'
         ),
         color=0xfffb00
     )
+    embed.set_author(name=old_nickname, url=jump_url)
     embed.set_thumbnail(url=DENIED_IMAGE_URL)
     return embed
 
